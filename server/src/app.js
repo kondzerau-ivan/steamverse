d0396614565be408c7d4d85008b607b5596e40e1
Ivan Kondzerau <konderev@aheadworks.com>
2025-05-09 12:11:55 +0300

    fix: update route usage for authRouter and profileRouter in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,21 @@
 // server/src/app.js
 import express from 'express';
 import { sessionMiddleware } from './middleware/sessionMiddleware.js';
-
-// Импортируем конфигурацию Passport — этот импорт выполнит настройку стратегии.
 import './passport/passportConfig.js';
 import passport from 'passport';
 import { authRouter } from './routes/authRouter.js';
+import { profileRouter } from './routes/profileRouter.js';
 
 const app = express();
 
-// Подключаем middleware для сессий
 app.use(sessionMiddleware);
 
-// Инициализируем Passport и его сессию
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Подключение маршрутов для аутентификации
-app.use(authRouter);
+app.use('/', authRouter);
+app.use('/', profileRouter);
 
-// Простой домашний маршрут для проверки работы сервера.
 app.get('/', (req, res) => {
   res.send('Привет! Если вы видите это сообщение, то Express-сервер успешно настроен.');
 });
