0b44a12159c83bda87e2a8909a734f2ea65c2b94
Ivan Kondzerau <konderev@aheadworks.com>
2025-05-09 12:02:17 +0300

    feat: add authentication routes for Steam integration

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -1,20 +1,18 @@
 // server/src/routes/authRoutes.js
 import { Router } from 'express';
-import passport from 'passport';
+import { passport } from '../passport/passportConfig.js'; // Импортируем конфигурацию Passport
 import { steamAuthController, steamReturnController } from '../controllers/authController.js';
 
-const router = Router();
+export const authRouter = Router();
 
 // Маршрут для начала аутентификации через Steam.
-router.get('/auth/steam',
+authRouter.get('/auth/steam',
   passport.authenticate('steam'),
   steamAuthController
 );
 
 // Маршрут для обратного вызова после аутентификации.
-router.get('/auth/steam/return',
-  passport.authenticate('steam', { failureRedirect: '/login' }),
+authRouter.get('/auth/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/' }),
   steamReturnController
 );
-
-export default router;
