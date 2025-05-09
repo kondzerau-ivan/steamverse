d51e0c0a58b4f22881c48f0b15384a613557654e
Ivan Kondzerau <konderev@aheadworks.com>
2025-05-09 12:03:25 +0300

    refactor: streamline Steam authentication logic in passportConfig.js

--- server/src/passport/passportConfig.js
+++ server/src/passport/passportConfig.js
@@ -1,24 +1,55 @@
 // server/src/passport/passportConfig.js
 import passport from 'passport';
 import SteamStrategy from 'passport-steam';
-import steamConfig from '../config/steam.js';
+import { returnURL, realm, apiKey } from '../config/steam.js';
+import { User } from '../models/User.js';
 
 passport.use(new SteamStrategy({
-  returnURL: steamConfig.returnURL,
-  realm: steamConfig.realm,
-  apiKey: steamConfig.apiKey
+  returnURL,
+  realm,
+  apiKey
 },
-  (identifier, profile, done) => {
-    profile.identifier = identifier;
-    return done(null, profile);
+  async (identifier, profile, done) => {
+    try {
+      const steamId = profile.id || identifier.split('/').pop();
+
+      let user = await User.findOne({ where: { steamId } });
+
+      if (!user) {
+        user = await User.create({
+          steamId,
+          nickName: profile.displayName || '',
+          realName: profile.realName || '',
+          avatar: profile.photos.length ? profile.photos[0].value : null,
+          lastLogin: new Date()
+        });
+      } else {
+        user.lastLogin = new Date();
+        await user.save();
+      }
+      return done(null, user);
+    } catch (error) {
+      console.error('Ошибка аутентификации через Steam:', error);
+      return done(error, null);
+    }
   }
 ));
 
 passport.serializeUser((user, done) => {
-  console.log(user)
-  done(null, user);
+  done(null, user.id);
 });
 
-passport.deserializeUser((user, done) => {
-  done(null, user);
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      return done(new Error("Пользователь не найден"), null);
+    }
+    done(null, user);
+  } catch (error) {
+    console.error('Ошибка при десериализации пользователя:', error);
+    done(error, null);
+  }
 });
+
+export { passport };
