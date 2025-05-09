71ec536c045ee26a47766e4eecb31e3849fd3174
Ivan Kondzerau <konderev@aheadworks.com>
2025-05-09 12:01:29 +0300

    refactor: change default export to named exports in steam.js

--- server/src/config/steam.js
+++ server/src/config/steam.js
@@ -1,6 +1,4 @@
 // server/src/config/steam.js
-export default {
-  returnURL: 'http://localhost:3000/auth/steam/return', // URL, куда Steam перенаправит пользователя
-  realm: 'http://localhost:3000/',                      // Базовый URL домена
-  apiKey: process.env.STEAM_API_KEY || 'DC21432BB3116395884B9DA72C4B2E97'
-};
+export const returnURL = 'http://localhost:3000/auth/steam/return'; // URL, куда Steam перенаправит пользователя
+export const realm = 'http://localhost:3000/';                      // Базовый URL домена
+export const apiKey = process.env.STEAM_API_KEY || 'DC21432BB3116395884B9DA72C4B2E97';
